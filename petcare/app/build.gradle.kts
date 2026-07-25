91103630e13662161668da9734f17987c85c7eef
silvamirandaheitor9-creator <silvamirandaheitor9@gmail.com>
2026-07-25 20:14:51 -0300

    chore: change package name from com.petcare.app to br.com.petingle

--- petcare/app/build.gradle.kts
+++ petcare/app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.petcare.app"
+    namespace = "br.com.petingle"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.petcare.app"
+        applicationId = "br.com.petingle"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
